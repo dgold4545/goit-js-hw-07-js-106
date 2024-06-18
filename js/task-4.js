c67a4686a3bd747af13a8093583dc6017c3a7ed3
Dmytro Holovskyi <80936904545dgold@gmail.com>
2024-06-18 21:05:37 +0300

    Update task-4
    Update task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,53 @@
+// Завдання 4
+// Напиши скрипт управління формою логіна.
 
+// <form class="login-form">
+//   <label>
+//     Email
+//     <input type="email" name="email" />
+//   </label>
+//   <label>
+//     Password
+//     <input type="password" name="password" />
+//   </label>
+//   <button type="submit">Log in</button>
+// </form>
+
+// відправлення форми form.login-form повинна відбуватися за подією submit.
+// Під час відправлення форми сторінка не повинна перезавантажуватися.
+// Якщо при сабміті у формі є незаповнені поля, виводь alert з попередженням про те, що 'All form fields must be filled in'. Не додавай на інпути атрибут required, валідація має відбуватися саме через JS.
+// Якщо користувач заповнив усі поля і відправив форму, збери значення полів в об'єкт з двома властивостями, де ключ — це ім'я інпутів, а значення — відповідні значення цих інпутів, очищені від пробілів по краях. Для доступу до елементів форми використовуй властивість elements.
+// При сабміті форми виведи об'єкт із введеними даними в консоль і очисти значення полів форми методом reset.
+
+// На що буде звертати увагу ментор при перевірці:
+
+// Прослуховується подія submit
+// Під час відправлення форми сторінка не перезавантажується
+// Якщо при сабміті у формі є незаповнені поля, виводиться alert
+// При сабміті в консоль виводиться об’єкт з двома властивостями, де ключі — це ім’я інпутів, а значення — відповідні значення цих інпутів, очищені від пробілів по краях
+// Після сабміту елементи форми очищаються
+
+const refs = {
+  formElem: document.querySelector(".login-form"),
+};
+
+refs.formElem.addEventListener("submit", cheakInputs);
+
+function cheakInputs(event) {
+  event.preventDefault();
+  const currentTarget = event.currentTarget;
+  console.dir(currentTarget);
+
+  const inputsValue = {
+    email: currentTarget.elements.email.value.trim(),
+    password: currentTarget.elements.password.value.trim(),
+  };
+
+  if (inputsValue.email === "" || inputsValue.password === "") {
+    alert("All form fields must be filled in");
+  }
+
+  console.log(inputsValue);
+
+  currentTarget.reset();
+}
